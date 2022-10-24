ed09ec275a2463a937dcbef7903c7e3108a5aa8a
Zbram <75671598+Zbram@users.noreply.github.com>
2022-10-24 17:48:18 +0200

    added subject
    added subject.java

---
+++ Subject.java
@@ -0,0 +1,12 @@
+public class Subject {
+    private String subjectCode, subjectDescription;
+    private double grade;
+
+    public double averageGrade(){
+        return 1;
+    }
+
+    public double averageGradePerSubjectAndDepartment(){
+        return 1;
+    }
+}
