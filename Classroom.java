68d96d2a8592b01de1591dd6fee1d5a71d05b3db
AngeleBlais <94390537+AngeleBlais@users.noreply.github.com>
2022-10-24 17:48:59 +0200

    commit classroom/teacher

---
+++ Classroom.java
@@ -0,0 +1,11 @@
+public class Classroom {
+    private String roomNumber;
+    private int size; 
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+    public int getSize() {
+        return size;
+    }
+}
---
+++ Teacher.java
@@ -0,0 +1,27 @@
+public class Teacher {
+    private String lastName;
+    private String firstName;
+    private String phone;
+    private String email;
+    private String dateOfRecruitment;
+    private String staffNumber;
+
+    public String getDateOfRecruitment() {
+        return dateOfRecruitment;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }   
+    public String getPhone() {
+        return phone;
+    }
+    public String getStaffNumber() {
+        return staffNumber;
+    }
+}
