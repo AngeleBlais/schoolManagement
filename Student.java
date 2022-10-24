9e6927907690aaae595bc469f4cf414f7df8729b
Skiepy <72888827+Skiepy@users.noreply.github.com>
2022-10-24 17:48:15 +0200

    commit student

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private String lastName;
+    private String firstName;
+    private String phone;
+    private String email;
+    private String yearRegistered;
+    private String studentNumber;
+
+    public Double averageGPA() {
+        return 1.0d;
+    }
+
+    public void listSubjectsWithNoGrade() { 
+    }
+}
